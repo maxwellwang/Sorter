f43e1ecd772349722811a3b6554800edd05e9d3e
Maxwell Wang <maxwellwang01@gmail.com>
2020-02-16 16:33:20 -0500

    introduced errno and perror

--- fileSort.c
+++ fileSort.c
@@ -3,8 +3,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
+#include <ctype.h>
 #define DEBUG 1
 
 int comparator(void* a, void* b) {
@@ -20,28 +20,35 @@ int quickSort(void* toSort, int (*comparator)(void*, void*)) {
 }
 
 int main(int argc, char* argv[]) {
+	//int (*compPtr)(void*, void*) = comparator;
+	
 	// handle input errors
 	if (argc != 3) {
 		printf("Fatal Error: Expected 2 arguments, had %d\n", argc - 1);
-		return 1;
+		exit(1);
 	}
 	
 	if (argv[1][0] != '-' || (argv[1][1] != 'i' && argv[1][1] != 'q') || argv[1][2] != '\0') {
-		printf("Fatal Error: \"%s\" is not a valid sort flag\n", argv[1]);
-		return 1;
+		printf("Fatal Error: \"%s\" is not a valid sort flag, enter \"-i\" for insertion sort or \"-q\" for quicksort\n", argv[1]);
+		exit(1);
 	}
 	int fd = open(argv[2], O_RDONLY);
 	if (fd == -1) {
-		printf("Fatal Error: File \"%s\" does not exist\n", argv[2]);
-		return 1;
+		perror("Fatal Error");
+		exit(1);
 	}
 	if (DEBUG) {
 		printf("Flag: %s\n", argv[1]);
 		printf("File Path: %s\n", argv[2]);
 	}
 	
+	// read from file
 	
-	close(fd);
+	
+	int status = close(fd);
+	if (status == -1) {
+		perror("Error");
+	}
 	
 	return 0;
 }
