35dc2e07af3fae930e44902e5b925afbdbcfa9eb
Maxwell Wang <maxwellwang01@gmail.com>
2020-02-15 21:48:06 -0500

    added function prototypes and replaced strcmp since it is not allowed

--- fileSort.c
+++ fileSort.c
@@ -3,8 +3,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
 #define DEBUG 1
+
+int comparator(void* a, void* b) {
+	return 0;
+}
+
+int insertionSort(void* toSort, int (*comparator)(void*, void*)) {
+	return 0;
+}
+			
+int quickSort(void* toSort, int (*comparator)(void*, void*)) {
+	return 0;
+}
 
 int main(int argc, char* argv[]) {
 	// handle input errors
@@ -12,7 +23,8 @@ int main(int argc, char* argv[]) {
 		printf("Fatal Error: Expected 2 arguments, had %d\n", argc - 1);
 		return 1;
 	}
-	if (strcmp(argv[1], "-i") && strcmp(argv[1], "-q")) {
+	
+	if (argv[1][0] != '-' || (argv[1][1] != 'i' && argv[1][1] != 'q') || argv[1][2] != '\0') {
 		printf("Fatal Error: Invalid flag entered, enter -i for insertion sort or -q for quicksort\n");
 		return 1;
 	}
@@ -21,7 +33,6 @@ int main(int argc, char* argv[]) {
 		printf("Fatal Error: Invalid file path entered\n");
 		return 1;
 	}
-	
 	if (DEBUG) {
 		printf("Flag: %s\n", argv[1]);
 		printf("File Path: %s\n", argv[2]);
