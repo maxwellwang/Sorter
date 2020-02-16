bb3192ce373d3f48860c5865bb763c6f5eb6362b
Maxwell Wang <maxwellwang01@gmail.com>
2020-02-16 16:05:32 -0500

    improved error handling

--- fileSort.c
+++ fileSort.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
 #define DEBUG 1
 
 int comparator(void* a, void* b) {
@@ -25,18 +27,21 @@ int main(int argc, char* argv[]) {
 	}
 	
 	if (argv[1][0] != '-' || (argv[1][1] != 'i' && argv[1][1] != 'q') || argv[1][2] != '\0') {
-		printf("Fatal Error: Invalid flag entered, enter -i for insertion sort or -q for quicksort\n");
+		printf("Fatal Error: \"%s\" is not a valid sort flag\n", argv[1]);
 		return 1;
 	}
 	int fd = open(argv[2], O_RDONLY);
 	if (fd == -1) {
-		printf("Fatal Error: Invalid file path entered\n");
+		printf("Fatal Error: File \"%s\" does not exist\n", argv[2]);
 		return 1;
 	}
 	if (DEBUG) {
 		printf("Flag: %s\n", argv[1]);
 		printf("File Path: %s\n", argv[2]);
 	}
+	
+	
+	close(fd);
 	
 	return 0;
 }
