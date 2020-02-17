406636cea48805a9f5527153bb588d58a9764321
Maxwell Wang <maxwellwang01@gmail.com>
2020-02-17 11:22:01 -0500

    improved reader but problem of buffer size stuck at 8

--- fileSort.c
+++ fileSort.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <string.h>
 #define DEBUG 1
 
 int comparator(void* a, void* b) {
@@ -20,23 +21,35 @@ int quickSort(void* toSort, int (*comparator)(void*, void*)) {
 }
 
 int main(int argc, char* argv[]) {
+	// buffer for reading one char at a time
 	char c = '?';
-	//int (*compPtr)(void*, void*) = comparator;
+	// buffer for elements
+	char* buffer = malloc(1);
+	if (buffer == NULL) {
+		printf("Malloc failed\n");
+		return 0;
+	}
+	// updated buffer which can hold one more char than previous buffer
+	char* nextBuffer;
+	int elementFound = 0;
+	int fileEmpty = 1;
+	int status = 0;
+	// int (*compPtr)(void*, void*) = comparator;
 	
 	// handle input errors
 	if (argc != 3) {
 		printf("Fatal Error: Expected 2 arguments, had %d\n", argc - 1);
-		exit(1);
+		return 0;
 	}
 	
 	if (argv[1][0] != '-' || (argv[1][1] != 'i' && argv[1][1] != 'q') || argv[1][2] != '\0') {
 		printf("Fatal Error: \"%s\" is not a valid sort flag, enter \"-i\" for insertion sort or \"-q\" for quicksort\n", argv[1]);
-		exit(1);
+		return 0;
 	}
 	int fd = open(argv[2], O_RDONLY);
 	if (fd == -1) {
 		perror("Fatal Error");
-		exit(1);
+		return 0;
 	}
 	if (DEBUG) {
 		printf("Flag: %s\n", argv[1]);
@@ -44,18 +57,58 @@ int main(int argc, char* argv[]) {
 	}
 	
 	// read from file
-	int bytesRead = read(fd, &c, 1);
-	if (bytesRead == 0) {
+	while (read(fd, &c, 1) > 0) {
+		if (fileEmpty) {
+			fileEmpty = 0;
+		}
+		
+		// assign type when integer/string is found, and create appropriate buffer
+		if (elementFound == 0 && isalpha(c)) {
+			elementFound = 1;
+		} else if (elementFound == 0 && isdigit(c)) {
+			elementFound = 1;
+		}
+		
+		if (isalpha(c) || isdigit(c) || c == ',') {
+			// realloc to allow one more char
+			if (DEBUG) {
+				printf("Size of buffer: %d\n", (int)(sizeof(buffer)));
+			}
+			nextBuffer = malloc(sizeof(buffer) + 1);
+			if (nextBuffer == NULL) {
+				printf("Malloc failed\n");
+				return 0;
+			}
+			memcpy(nextBuffer, buffer, sizeof(buffer) + 0);
+			free(buffer);
+			buffer = nextBuffer;
+			nextBuffer = NULL;
+			
+			sprintf(buffer + sizeof(buffer) - 1, "%c", c);
+			if (DEBUG) {
+				printf("Printed %c at position %d\n", c, (int)(sizeof(buffer) - 1));
+			}
+		}
+	}
+	if (fileEmpty) {
 		printf("Warning: File is empty\n");
 	}
-	while (read(fd, &c, 1) > 0) {
-		printf("%c\n", c);
+	if (!elementFound) {
+		printf("Warning: No integers/strings found in file\n");
 	}
 	
-	int status = close(fd);
+	if (DEBUG) {
+		int i;
+		for (i = 0; i < sizeof(buffer); i++) {
+			printf("%c", buffer[i]);
+		}
+	}
+	
+	status = close(fd);
 	if (status == -1) {
 		perror("Error");
 	}
 	
+	free(buffer);
 	return 0;
 }
