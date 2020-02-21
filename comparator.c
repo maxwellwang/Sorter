749b4161d03a3d25e70205e49bb59293e0b19def
scheng <scheng2019@gmail.com>
2020-02-21 17:27:51 -0500

    Fixed quick sort

--- comparator.c
+++ comparator.c
@@ -1,102 +1,91 @@
 #include <stdio.h>
 
-
 typedef struct node_tmp {
   int value;
   struct node_tmp * next;
 } Node;
 
+void print(Node * head) {
+  Node * db = head;
+  for (; db != NULL; db = db->next) {
+    printf("%d ", db->value);
+  }
+  printf("\n");
+}
 
-//int comparator(void * node1, node * node2) {
-//  char * num = "0123456789-";
-//  //empty string?
-//  int i = 0;
-//  int isNum = 0;
-//  for (; i < 11; i++) {
-//    if (num[i] == (((Node *)node1)->data)[0]) {
-//      isNum = 1;
-//      break;
-//    }
-//  }
-//
-//  if (isNum) {
-//    if (((Node *) node1)->data == ((Node *) node2)->data) {
-//      return 0;
-//    }
-//    return ((Node *) node1)->data > ((Node *) node2)->data ? 1 : -1;
-//  }
-//}
-//
-Node * partition(Node * head, Node * end, Node ** nextHead, Node ** nextEnd) {
+Node * partition(Node * head, Node * end) {
   Node * pvt = head;
+  int pivot = head->value;
   Node * i = head;
   Node * j;
   int tmp;
+
   for (j = head; j != NULL; j = j->next) {
-    if (j->value < pvt->value) {
+    if (j->value < pivot) {
+      if (i == pvt) {
+	pvt = j;
+      }
       tmp = j->value;
-      j->value = pvt->value;
-      pvt->value = tmp;
+      j->value = i->value;
+      i->value = tmp;
       i = i->next;
     }
   }
-  tmp = pvt->value;
-  i->value = pvt->value;
-  pvt->value = tmp;
-  (*nextHead) = head;
-  (*nextEnd) = end;
+  pvt->value = i->value;
+  i->value = pivot;
   return i;
 }
 
 Node * quickSortHelper(Node * head, Node * end) {
   if (head == NULL || head->next == NULL) {
-    return head;
+      return head;
   }
-
-  Node * nextHead;
-  Node * nextEnd;
-  Node * ptr = partition(head, end, &nextHead, &nextEnd);
-
-  Node * tmp = nextHead;
-  while (tmp->next != end) {
-    printf("1");
-    tmp = tmp->next;
-  }
-  tmp->next = NULL;
-  nextHead = quickSortHelper(nextHead, tmp);
-  tmp = nextHead;
-  while (tmp->next != NULL) {
-    tmp = tmp->next;
-  }
-  tmp->next = ptr;
   
-  ptr->next = quickSortHelper(ptr->next, nextEnd);
-
-  return nextHead;
+  Node * ptr = partition(head, end);
+  Node * tmp = head;
+  
+  if (tmp != ptr) { //need this check?
+    while (tmp->next != ptr) {
+      tmp = tmp->next;
+    }
+    tmp->next = NULL;
+    head = quickSortHelper(head, tmp);
+    tmp = head;
+    while (tmp->next != NULL) {
+      tmp = tmp->next;
+    }
+    tmp->next = ptr;
+  }
+  ptr->next = quickSortHelper(ptr->next, end);
+  return head;
 }
+
 
 int quickSort(Node ** head) {
   if ((*head) == NULL) {
     return -1;
   }
-  Node * ptr = (*head)->next;
-  while (ptr != NULL) {
-    printf("2");
+  Node * ptr = (*head);
+  while (ptr->next != NULL) {
     ptr = ptr->next;
   }
   *head = quickSortHelper(*head, ptr);
   return 1;
 }
 
-int main() {
-  Node a = {1, NULL};
-  Node b = {2, &a};
-  Node c = {3, &b};
-  Node * tmp = &c;
-  quickSort(&tmp);
-  Node * ptr = tmp;
-  for (; ptr != NULL; ptr = ptr->next) {
-    printf("%d ", ptr->value);
+int main(int argc, char * argv[]) {
+  Node a = {8, NULL};
+  Node b = {1, &a};
+  Node c = {1, &b};
+  Node d = {19999, &c};
+  Node e = {1, &d};
+  Node f = {-1, &e};
+  Node g = {50, &f};
+  Node h = {7, &g};
+  Node * tmp = &h;
+  if (quickSort(&tmp)) {
+    Node * ptr = tmp;
+    print(tmp);
   }
   return 1;
 }
